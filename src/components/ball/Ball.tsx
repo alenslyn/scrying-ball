12b1421322e30d6d1d722312a0affffec27c6cf9
alenslyn <alenslyn@gmail.com>
2024-06-06 13:01:37 +0200

    feat: add re-roll button and styles

--- src/components/ball/Ball.tsx
+++ src/components/ball/Ball.tsx
@@ -1,25 +1,50 @@
 import { useState } from "react";
+import { IoMdRefresh } from "react-icons/io";
 import "./Ball.css";
 
-const Ball = () => {
+const Ball: React.FC = () => {
   const [isPressed, setIsPressed] = useState(false);
   const [text, setText] = useState("Well...");
+  const [pressCount, setPressCount] = useState(0);
+  const [showReRoll, setShowReRoll] = useState(false);
 
   const handleClick = () => {
-    setIsPressed(true);
-    setText(text === "Well..." ? "Try again?" : "Again?");
-    setTimeout(() => {
-      setIsPressed(false);
-    }, 200);
+    if (pressCount < 2) {
+      setPressCount((prevCount) => prevCount + 1);
+      setIsPressed(true);
+      setText((prevText) => (prevText === "Well..." ? "Try again?" : "Again?"));
+      setTimeout(() => {
+        setIsPressed(false);
+      }, 220);
+    } else {
+      setText("Here is your answer!");
+      setShowReRoll(true);
+    }
+  };
+
+  const handleReRoll = () => {
+    setPressCount(0);
+    setText("Well...");
+    setShowReRoll(false);
+    setIsPressed(false);
   };
 
   return (
     <div className="ball-container">
-      <div
-        className={`ball ${isPressed ? "ball-pressed" : ""}`}
-        onClick={handleClick}
-      ></div>
-      <p className="ball-text">{text}</p>
+      <div className="ball-inner">
+        <div
+          className={`ball ${isPressed ? "ball-pressed" : ""} ${
+            showReRoll ? "ball-big" : ""
+          }`}
+          onClick={handleClick}
+        ></div>
+        <p className="ball-text">{text}</p>
+      </div>
+      {showReRoll && (
+        <div className="reroll-icon" onClick={handleReRoll}>
+          <IoMdRefresh />
+        </div>
+      )}
     </div>
   );
 };
